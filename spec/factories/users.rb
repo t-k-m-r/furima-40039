83d1e34d2d438dd0d399f7ef4a43942788736d03
t-k-m-r <tn0vk14r107252z2t@gmail.com>
2023-11-24 15:36:42 +0900

    ファクトリーボットuser.password修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Lorem.word }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
     last_name             { Faker::Name.last_name }
     first_name            { Faker::Name.first_name }
