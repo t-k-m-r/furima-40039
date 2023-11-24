75fbc4de73bcca9b7615ed97089424a56f59a4d5
t-k-m-r <tn0vk14r107252z2t@gmail.com>
2023-11-24 15:34:48 +0900

    バリデーションに伸ばし棒追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,11 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
 
   validates :last_name,        presence: true,
-                               format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
+                               format: { with: /\A[ぁ-んーァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
   validates :first_name,       presence: true,
-                               format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
+                               format: { with: /\A[ぁ-んーァ-ヶ一-龥々]+\z/, message: 'is invalid. Input full-width characters' }
   validates :last_name_kana,   presence: true,
-                               format: { with: /\A[ァ-ヶ]+\z/, message: 'is invalid. Input full-width katakana characters' }
+                               format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
   validates :first_name_kana,  presence: true,
-                               format: { with: /\A[ァ-ヶ]+\z/, message: 'is invalid. Input full-width katakana characters' }
+                               format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
 end
