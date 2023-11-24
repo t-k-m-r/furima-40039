6ba885fe99f5639914adb0a12cac1f1cca753909
t-k-m-r <tn0vk14r107252z2t@gmail.com>
2023-11-24 15:40:53 +0900

    Rubocop自動修正実行

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -94,25 +94,25 @@ RSpec.describe User, type: :model do
       it 'last_nameが全角(漢字・ひらがな・カタカナ)以外では登録できない' do
         @user.last_name = 'ａｂｃ１２３'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name is invalid. Input full-width characters")
+        expect(@user.errors.full_messages).to include('Last name is invalid. Input full-width characters')
       end
 
       it 'last_nameが半角カタカナでは登録できない' do
         @user.last_name = 'ｶﾀ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Last name is invalid. Input full-width characters")
+        expect(@user.errors.full_messages).to include('Last name is invalid. Input full-width characters')
       end
 
       it 'first_nameが全角(漢字・ひらがな・カタカナ)以外では登録できない' do
         @user.first_name = 'ａｂｃ１２３'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name is invalid. Input full-width characters")
+        expect(@user.errors.full_messages).to include('First name is invalid. Input full-width characters')
       end
 
       it 'first_nameが半角カタカナでは登録できない' do
         @user.first_name = 'ｶﾅ'
         @user.valid?
-        expect(@user.errors.full_messages).to include("First name is invalid. Input full-width characters")
+        expect(@user.errors.full_messages).to include('First name is invalid. Input full-width characters')
       end
 
       it 'last_name_kanaが空では登録できない' do
