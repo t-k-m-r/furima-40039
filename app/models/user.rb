fde10b730e67e52b7512f75e45b8b6dfef7c9e60
t-k-m-r <tn0vk14r107252z2t@gmail.com>
2023-11-29 14:49:57 +0900

    購入履歴テーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many  :items
+  has_one   :purchase_history
 
   validates :nickname, presence: true
   validates :birth_date, presence: true
