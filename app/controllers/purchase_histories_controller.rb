ea4f1023bd4d1e221c3edefafc8a2e6df21d96d7
t-k-m-r <tn0vk14r107252z2t@gmail.com>
2023-11-30 23:28:29 +0900

    クレジットカード決済機能実装

--- app/controllers/purchase_histories_controller.rb
+++ app/controllers/purchase_histories_controller.rb
@@ -2,6 +2,7 @@ class PurchaseHistoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @purchase_history_address = PurchaseHistoryAddress.new
     @item = Item.find_by(id: params[:item_id])
     if current_user.id == @item.user_id || PurchaseHistory.find_by(item_id: @item.id) != nil
@@ -10,12 +11,14 @@ class PurchaseHistoriesController < ApplicationController
   end
 
   def create
+    @item = Item.find_by(id: params[:item_id])
     @purchase_history_address = PurchaseHistoryAddress.new(address_params)
     if @purchase_history_address.valid?
+      pay_item
       @purchase_history_address.save
       redirect_to root_path
     else
-      @item = Item.find_by(id: params[:item_id])
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
@@ -23,6 +26,16 @@ class PurchaseHistoriesController < ApplicationController
   private
 
   def address_params
-    params.require(:purchase_history_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_history_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: address_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
