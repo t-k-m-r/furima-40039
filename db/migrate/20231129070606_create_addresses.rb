fd93b3058161fb66dafb804fbf82096171cd5123
t-k-m-r <tn0vk14r107252z2t@gmail.com>
2023-12-01 14:15:02 +0900

    migrateファイル修正

--- db/migrate/20231129070606_create_addresses.rb
+++ db/migrate/20231129070606_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[7.0]
       t.integer    :prefecture_id,    null: false
       t.string     :city,             null: false
       t.string     :address,          null: false
-      t.string     :building,         null: false
+      t.string     :building
       t.string     :phone_number,     null: false
       t.references :purchase_history, null: false, foreign_key: true
 
