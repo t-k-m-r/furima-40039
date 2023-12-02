782f4235a17c81071f3fc17a848db5839e1d3650
t-k-m-r <tn0vk14r107252z2t@gmail.com>
2023-12-02 22:04:29 +0900

    jsファイルにif文追加

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,12 +1,14 @@
 function price (){
-  const itemPrice  = document.getElementById("item-price");
-  itemPrice.addEventListener("keyup", () => {
-    const item_price = itemPrice.value;
-    const addTax  = document.getElementById("add-tax-price");
-    addTax.innerHTML = Math.floor(item_price * 0.1).toLocaleString();
-    const profit  = document.getElementById("profit");
-    profit.innerHTML = Math.ceil(item_price * 0.9).toLocaleString();
-  });
+  if(null != document.getElementById("item-price")) {
+    const itemPrice  = document.getElementById("item-price");
+    itemPrice.addEventListener("keyup", () => {
+      const item_price = itemPrice.value;
+      const addTax  = document.getElementById("add-tax-price");
+      addTax.innerHTML = Math.floor(item_price * 0.1).toLocaleString();
+      const profit  = document.getElementById("profit");
+      profit.innerHTML = Math.ceil(item_price * 0.9).toLocaleString();
+    })
+  };
 };
 
 window.addEventListener('turbo:load', price);
